ba9405dc2dc490c431ecd7323944720fd770f311
Yatin Nagpal <nnega4428@gmail.com>
2023-03-15 18:25:12 +1100

    testing render

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -3,17 +3,6 @@ module.exports = {
   description: 'replies with pong',
   execute(msg, args) {
 
-    if (msg.member.roles.cache.has('1071266290288840734')) {
-      msg.channel.send('pong')
-    }else {
-      msg.channel.send('Sorry, you dont have right permissions');
-      // msg.reply('sup')
-      // console.log(msg.member.roles.cache.some(role => role.name === 'Mod'));
-      // msg.member.roles.add('').catch(console.error)
-    }
-
-    if ( msg.member.permissions.has('KICK_MEMBERS')){
-      msg.channel.send('You have permission to kick members')
-    }
+      msg.channel.send('ping pong ping pong')
   }
 };
--- src/index.js
+++ src/index.js
@@ -47,7 +47,7 @@ client.on("messageCreate", (msg) => {
   }
 });
 
-let scheduledMessage = new cron.CronJob("*/20 * * * *", () => {
+let scheduledMessage = new cron.CronJob("*/29 * * * *", () => {
   client.commands.get('fuel').execute(client)
 });
 
